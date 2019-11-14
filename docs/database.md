339b73d561c7d3805b99f71888e5686c99ae3238
dgisolfi <daniel.gisolfi1@marist.edu>
2019-11-14 11:42:00 -0500

    Setup basic site and added API docs, theres a lot of typos, I know

---
+++ docs/database.md
@@ -0,0 +1,16 @@
+---
+id: database
+title: Database
+sidebar_label: Database
+---
+
+The war tool uses a postgres database to store and process all relavant user and sentence information.
+
+## Defintion
+
+All the nessecary sql commands to create a new instance of the War database can be found within the `database` directory of the [API repository](https://github.com/Capping-WAR/API/tree/master/database)
+
+## Data Backup/Recovery
+
+
+## Deployment
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,37 +11,35 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: 'dgisolfi',
     // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
+    // if it is not '/', like: '/test-siteimg/image.jpg'.
+    image: 'img/undraw_open_source.svg',
     infoLink: 'https://www.facebook.com',
     pinned: true,
   },
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'WAR Documentation', // Title for your website.
+  tagline: 'A Docusaurus site dedicated to the documentation of the WAR Infastructure/Tool',
+  url: 'http://war.marist.ai', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'WarDocs',
+  organizationName: 'WAR',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'project_overview', label: 'Project'},
+    {doc: 'developer_overview', label: 'Developer'},
   ],
 
   // If you have users set above, you add it here:
@@ -54,8 +52,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#733186',
-    secondaryColor: '#50225d',
+    primaryColor: '#358ece',
+    secondaryColor: '#358ece',
   },
 
   /* Custom fonts for website */
@@ -73,7 +71,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Daniel Gisolfi - War`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
